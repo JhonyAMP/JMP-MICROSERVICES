bbb5ed8db23ddf82f58afedb4c8d72ff71bb0eb4
JhonyAMP <119316944+JhonyAMP@users.noreply.github.com>
2025-04-21 22:10:38 -0500

    Delete distribuidas_repaso directory

--- distribuidas_repaso/jmp-estudiante/src/main/java/com/jmp/jmpestudiante/Repositorio/ApoderadoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.jmp.jmpestudiante.Repositorio;
-
-import com.jmp.jmpestudiante.entidad.Apoderado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApoderadoRepositorio extends JpaRepository<Apoderado, Long> {
-}
-
--- distribuidas_repaso/jmp-estudiante/src/main/java/com/jmp/jmpestudiante/Repositorio/EstudianteRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.jmp.jmpestudiante.Repositorio;
-
-import com.jmp.jmpestudiante.entidad.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jmp.jmpestudiante.Repositorio.EstudianteRepositorio;
-
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Long> {
-}
