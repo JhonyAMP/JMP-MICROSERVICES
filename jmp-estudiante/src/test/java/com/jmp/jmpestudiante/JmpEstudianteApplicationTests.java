2973f7bfd37b8712d1e7a3dae380e199bd003a07
JhonyAMP <119316944+JhonyAMP@users.noreply.github.com>
2025-04-21 22:11:27 -0500

    Add files via upload

---
+++ jmp-estudiante/src/test/java/com/jmp/jmpestudiante/JmpEstudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmp.jmpestudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmpEstudianteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
